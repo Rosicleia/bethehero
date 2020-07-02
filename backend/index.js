7039c842fbbd62109cd9fe35c78a3975bf2cb1cb
Rosicleia <rosicleiasales@hotmail.com>
2020-07-01 23:15:15 -0300

    backend

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,47 @@ const express = require ('express');
 //Essa aplicação que depois vai ter todas as rotas, toda a parte de funcionalidade
 const app = express();
 
+//Informar que vai utilizar JSON no corpo de toas as requisições
+//Tem que vim antes das rotas
+//Aqui estou falando para o express ir la no corpo da minha requisição e converter oo JSON em objeto javascript, transformar em algo entendivel pela aplicação
+app.use(express.json());
+
 
 //Para criar primeira rota, quando coloca só a / é porque quero a rota raiz
 //Passa uma função como segundo parametro que recebe dois parametros requisição e resposta
-app.get('/', (request, response) => {
+//Rota é o caminho completo
+//Recuso o que vem depois da barra e geralmente vai estar associado a uma tabela do banco
+/*
+    Metodo HTTP
+    - GET: Utiliza para buscar uma informação do backend
+    - POST: Utiliza para criar uma informação do backend
+    - PUT: Utiliza para alterar uma informação do backend
+    - DELETE: Utiliza para deletar uma informação do backend
+*/
+
+/*
+    Tipos de Parâmetros
+    - QUERY PARAMS: Parâmetro nomeados enviados na rota após "?", geralmente servem para filtros, paginação
+    - ROUTE PARAMS: Parâmetro utilizado para identificar recursos (o que vem depois da /tabelas)
+    - REQUEST BODY: Corpo da requisição utilizado para criar(POST) ou alterar(PUT) recursos
+*/
+
+//Para acessar os parâmetros dentro da rota utiliza o resquest e response
+//Request: Guarda todos os dados que vem atraves da nossa requisição (usuario) em formato JSON
+//Response: Retorna uma resposta para o usuário em formato JSON
+//Para acessar utilizando Query:app.get('/users', (request, response) => {const params = request.query;...
+//Para acessar utilizando Route: app.get('/users/:id', (request, response) => {const params = request.params;...
+//Para acessar utilizando Request Body: app.post('/users', (request, response) => {const body = request.body;...
+app.post('/users', (request, response) => {
+
+    //Acessar parametros
+    const body = request.body;
+    console.log(body);
+
+    //Response: Retorna uma resposta para o usuário em formato JSON
     return response.json({
         evento: 'Semana OmniStack 11.0',
-        aluno: 'Rosicléia Sales'
+        aluno: 'Rosicleia'
     })
 });
 
